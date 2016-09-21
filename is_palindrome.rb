506a07ca1e8a8255766e03d42cd07da72d8430d8
Allison Patterson <allison.patterson@students.olin.edu>
2016-09-21 14:36:50 -0700

    is_palindrome in ruby!

--- is_palindrome.rb
+++ is_palindrome.rb
@@ -1,14 +1,32 @@
+#
+# Returns true if input str is a
+# palindrome, case insensitive.
+# Expects input str to be a string.
+#
+
 def is_palindrome(str)
-  str_array = str.split(//)
+  # check for and handle wonky inputs
+  if !str.is_a?(String)
+    puts "Whoops: Input str must be a string."
+    return
+  end
+
+  # represent str as an array of letters
+  str_array = str.downcase.split(//)
+
+  # compare forwards and backwards
   if str_array == str_array.reverse
     return true
   end
+
+  # if all else fails
   return false
+  
 end
 
 # some general proof of concept
-puts is_palindrome('racecar')
+puts is_palindrome('kayak')
 puts is_palindrome('nope')
+puts is_palindrome('RaCEcAr')
 puts is_palindrome('')
-puts is_palindrome('n')
-# puts is_palindrome()
+puts is_palindrome(123)
